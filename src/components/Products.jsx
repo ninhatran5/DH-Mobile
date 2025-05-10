89cbabe7d42a96b79c5dc5bdbaabdcdd6688eaf3
LeNguyenXeng <tungnguyenle0909@gmail.com>
2025-05-10 18:31:44 +0700

    feature: implement home screen

---
+++ src/components/Products.jsx
@@ -0,0 +1,173 @@
+import { FaRegHeart, FaShippingFast } from "react-icons/fa";
+import iphone from "../assets/images/iphone-16-pro-max.webp";
+import { Link } from "react-router-dom";
+import { FaArrowDownShortWide, FaArrowUpWideShort } from "react-icons/fa6";
+import "../assets/css/products.css";
+export default function Products({
+  title,
+  showHeader = true,
+  padding,
+  filter = true,
+}) {
+  const products = [
+    {
+      name: "iPhone 16 Pro Max 256GB | Chính hãng VN/A",
+      price: "27.890.000đ",
+      image: iphone,
+    },
+    {
+      name: "Samsung Galaxy S22 Ultra 512GB",
+      price: "29.990.000đ",
+      image: iphone,
+    },
+    {
+      name: "Samsung Galaxy S22 Ultra 512GB",
+      price: "29.990.000đ",
+      image: iphone,
+    },
+    {
+      name: "Samsung Galaxy S22 Ultra 512GB",
+      price: "29.990.000đ",
+      image: iphone,
+    },
+    {
+      name: "Samsung Galaxy S22 Ultra 512GB",
+      price: "29.990.000đ",
+      image: iphone,
+    },
+    {
+      name: "Samsung Galaxy S22 Ultra 512GB",
+      price: "29.990.000đ",
+      image: iphone,
+    },
+    {
+      name: "Samsung Galaxy S22 Ultra 512GB",
+      price: "29.990.000đ",
+      image: iphone,
+    },
+    {
+      name: "Samsung Galaxy S22 Ultra 512GB",
+      price: "29.990.000đ",
+      image: iphone,
+    },
+    {
+      name: "Samsung Galaxy S22 Ultra 512GB",
+      price: "29.990.000đ",
+      image: iphone,
+    },
+    {
+      name: "Samsung Galaxy S22 Ultra 512GB",
+      price: "29.990.000đ",
+      image: iphone,
+    },
+  ];
+  return (
+    <>
+      <section className={padding}>
+        <div className="container-fluid">
+          {filter && (
+            <div className="filter-bar-wrapper">
+              <div className="filter-extended">
+                <div className="filter-group">
+                  <label>Thương hiệu:</label>
+                  <select>
+                    <option value="">Tất cả</option>
+                    <option value="iphone">iPhone</option>
+                    <option value="samsung">Samsung</option>
+                    <option value="xiaomi">Xiaomi</option>
+                  </select>
+                </div>
+                <div className="filter-group">
+                  <label>Khoảng giá:</label>
+                  <select>
+                    <option value="">Tất cả</option>
+                    <option value="duoi-10tr">Dưới 10 triệu</option>
+                    <option value="10-20tr">10 - 20 triệu</option>
+                    <option value="tren-20tr">Trên 20 triệu</option>
+                  </select>
+                </div>
+              </div>
+              <div className="filter-buttons">
+                <button>
+                  <span>
+                    <FaArrowUpWideShort />
+                  </span>
+                  Giá Thấp - Cao
+                </button>
+                <button>
+                  <span>
+                    <FaArrowDownShortWide />
+                  </span>
+                  Giá Cao - Thấp
+                </button>
+                <button>
+                  <span>
+                    <FaShippingFast />
+                  </span>
+                  Sẵn hàng
+                </button>
+              </div>
+            </div>
+          )}
+          <div className="row">
+            <div className="col-md-12">
+              <div className="bootstrap-tabs product-tabs">
+                {showHeader && (
+                  <div className="tabs-header d-flex justify-content-between border-bottom my-5">
+                    <h3>{title}</h3>
+                    <nav>
+                      <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                        <Link
+                          to={"/products"}
+                          className="nav-link text-uppercase fs-6 active"
+                        >
+                          Đi đến shop →
+                        </Link>
+                      </div>
+                    </nav>
+                  </div>
+                )}
+                <div className="tab-content" id="nav-tabContent">
+                  <div
+                    className="tab-pane fade show active"
+                    id="nav-all"
+                    role="tabpanel"
+                    aria-labelledby="nav-all-tab"
+                  >
+                    <div className="product-grid row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
+                      {products.map((product, index) => (
+                        <div className="col" key={index}>
+                          <div className="product-item">
+                            <a href="#" className="btn-wishlist">
+                              <FaRegHeart style={{ fontSize: 20 }} />
+                            </a>
+                            <figure>
+                              <Link to={""} title={product.name}>
+                                <img
+                                  src={product.image}
+                                  className="tab-image"
+                                />
+                              </Link>
+                            </figure>
+                            <h3>{product.name}</h3>
+                            <span className="price">{product.price}</span>
+                            <div className="d-flex align-items-center justify-content-between">
+                              <a href="#" className="nav-link">
+                                Add to Cart
+                                <iconify-icon icon="uil:shopping-cart" />
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ src/components/SliderLogoBrand.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import Slider from "react-slick";
+import tecnoLogo from "../assets/images/Tecno_Mobile_logo.svg.png";
+import vivoLogo from "../assets/images/Vivo_logo_2019.svg.png";
+import honorLogo from "../assets/images/LOGO_Honor.svg.png";
+import redmagicLogo from "../assets/images/redmagic-logo.png.webp";
+import nokiaLogo from "../assets/images/nokia-new-logo-2023-png-vector.svg";
+import asusLogo from "../assets/images/AsusTek-black-logo.png";
+import sonyLogo from "../assets/images/sony.png";
+import masstelLogo from "../assets/images/Masstel-logo.png";
+import bkav from "../assets/images/logo-bkav.png";
+import appleLogo from "../assets/images/logo-apple.webp";
+import vnpayLogo from "../assets/images/logo-vndpay.webp";
+import samsungLogo from "../assets/images/logo-samsung.svg";
+
+import "../assets/css/slider.css";
+
+const SliderLogoBrand = () => {
+  const settings = {
+    infinite: true,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3800, // Giảm tốc độ chuyển động
+    autoplaySpeed: 100, // Thay đổi tốc độ chuyển tiếp
+    cssEase: "ease-in-out",
+    arrows: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  };
+
+  const logos = [
+    bkav,
+    vnpayLogo,
+    samsungLogo,
+    appleLogo,
+    tecnoLogo,
+    vivoLogo,
+    honorLogo,
+    redmagicLogo,
+    nokiaLogo,
+    asusLogo,
+    sonyLogo,
+    masstelLogo,
+  ];
+
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="partner-page">
+          <div className="slide-partner">
+            <div className="partner">
+              <p className="partner-title text-center">ĐỐI TÁC VÀ KHÁCH HÀNG</p>
+              <p className="partner-des text-center">PARTNERS AND CUSTOMERS</p>
+            </div>
+            <div className="slider-container">
+              <Slider {...settings} key={logos.length}>
+                {logos.map((logo, index) => (
+                  <div key={index} className="logo-slide">
+                    <img
+                      src={logo}
+                      alt={`Brand logo ${index + 1}`}
+                      className="logo-img"
+                    />
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </div>
+          <Slider {...{ ...settings, rtl: true }} key={logos.length + 1}>
+            {logos.map((logo, index) => (
+              <div key={index} className="logo-slide">
+                <img
+                  src={logo}
+                  alt={`Brand logo ${index + 1}`}
+                  className="logo-img"
+                />
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SliderLogoBrand;
