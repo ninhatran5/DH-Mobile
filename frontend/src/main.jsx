b5a0055a70c88e0caab26505f2c0616ee8c345da
LeNguyenXeng <tungnguyenle0909@gmail.com>
2025-05-13 13:43:16 +0700

    update: update product detail screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <I18nextProvider i18n={i18n}>
       <App />
+      <ToastContainer />
     </I18nextProvider>
-    <ToastContainer />
   </BrowserRouter>
 );
