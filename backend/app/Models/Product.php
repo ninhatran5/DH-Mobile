5c88059277aebb8188078c8f03ae36460114d608
ninhatran5 <ninhtnph49084@gmail.com>
2025-05-15 18:16:27 +0700

    feat: add api product crud and trash, restore from trash, delete permanently

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     //
+    use SoftDeletes;
+    protected $table = 'products';
+    protected $primaryKey = 'product_id';
+    protected $fillable = [
+        'name',
+        'category_id',
+        'description',
+        'image_url',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'category_id');
+    }
 }
