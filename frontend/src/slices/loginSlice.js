0bf617cb36691b58dac972cb6552a8c4819fbc88
LeNguyenXeng <tungnguyenle0909@gmail.com>
2025-05-19 13:57:16 +0700

    feat: login

---
+++ frontend/src/slices/loginSlice.js
@@ -0,0 +1,48 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { axiosConfig } from "../../utils/axiosConfig";
+
+const initialState = {
+  loginInitial: null,
+  loading: false,
+  error: null,
+};
+
+///CALL API LOGIN
+export const fetchLogin = createAsyncThunk(
+  "login/fetchLogin",
+  async (data, thunkAPI) => {
+    try {
+      const response = await axiosConfig.post("/login", data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response?.data?.message || "Đã có lỗi xảy ra"
+      );
+    }
+  }
+);
+export const loginSlice = createSlice({
+  name: "login",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // pending(đang call)
+      .addCase(fetchLogin.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      // call thành công
+      .addCase(fetchLogin.fulfilled, (state, action) => {
+        state.loading = false;
+        state.loginInitial = action.payload;
+      })
+      // call lỗi
+      .addCase(fetchLogin.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload || "Đã có lỗi xảy ra";
+      });
+  },
+});
+
+export default loginSlice.reducer;
--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import homeReducer from "../slices/homeSlice";
+import registerReducer from "../slices/registerSlice";
 
 export const store = configureStore({
   reducer: {
     home: homeReducer,
+    register: registerReducer,
   },
 });
