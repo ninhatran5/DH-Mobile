43522b02c9e5cb3648ec44b20880d504bfefbb07
LeNguyenXeng <tungnguyenle0909@gmail.com>
2025-05-19 14:17:21 +0700

    feat: forgot password function

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import homeReducer from "../slices/homeSlice";
 import registerReducer from "../slices/registerSlice";
+import loginReducer from "../slices/loginSlice";
+import forgotPasswordReducer from "../slices/forgotPasswordSlice";
 
 export const store = configureStore({
   reducer: {
     home: homeReducer,
     register: registerReducer,
+    login: loginReducer,
+    forgotPassword: forgotPasswordReducer,
   },
 });
