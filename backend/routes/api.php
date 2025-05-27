3e670008fff51d09373e7c1c7ee789c4041a91d9
HoangQuan <104688578+chapilcheapz@users.noreply.github.com>
2025-05-28 00:15:03 +0700

    feat: Done api delete all product on  cart
    * fix: list product variants
    
    * feat: Done api delete all product on  cart

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -200,7 +200,7 @@ Route::get('getCart', [CartItemController::class, 'getCart']);
 // // xóa sản phẩm khỏi giỏ hàng
 Route::delete('cart/remove/{id}', [CartItemController::class, 'removeProductFromCart']);
 // // xóa toàn bộ giỏ hàng
-// Route::delete('cart/clear', [CartItemController::class, 'clearCart']);
+Route::delete('cart/clear', [CartItemController::class, 'clearCart']);
 
 
 });
