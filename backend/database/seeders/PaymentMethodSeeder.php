96a65488de6212062210d72ecab923509f6aa699
HoangQuan <104688578+chapilcheapz@users.noreply.github.com>
2025-06-09 11:32:28 +0700

    update:PaymentMethodSeeder

---
+++ backend/database/seeders/PaymentMethodSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Payment_methods;
+use Illuminate\Database\Seeder;
+
+class PaymentMethodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $paymentMethods = [
+
+            [
+                'name' => 'VNPay',
+                'description' => 'Thanh toán qua VNPay',
+                'is_active' => true,
+            ],
+            [
+                'name' => 'COD',
+                'description' => 'Thanh toán COD',
+                'is_active' => true,
+            ],
+        ];
+
+        foreach ($paymentMethods as $method) {
+            Payment_methods::create($method);
+        }
+    }
+}
