9ea6a5acfa727f6aaab315df34f085957beee74a
ninhatran5 <ninhtnph49084@gmail.com>
2025-06-09 09:51:03 +0700

    feat: add api/orderController.

---
+++ backend/app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Orders;
+use App\Models\OrderItems;
+use App\Models\Product;
+
+class OrderController extends Controller
+{
+    //
+    public function createCODOrder(Request $request) {
+        
+    }
+}
