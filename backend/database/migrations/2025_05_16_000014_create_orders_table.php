65d01820fddf3832caee185694a9b0b1949feb5c
ninhatran5 <ninhtnph49084@gmail.com>
2025-06-16 17:35:33 +0700

    feat: increase the size of the price of the order board

--- backend/database/migrations/2025_05_16_000014_create_orders_table.php
+++ backend/database/migrations/2025_05_16_000014_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
             $table->string('order_code', 50)->unique();
             $table->unsignedBigInteger('user_id')->nullable();
             $table->unsignedBigInteger('method_id')->nullable();
-            $table->decimal('total_amount', 10, 2)->nullable();
+            $table->decimal('total_amount', 11, 2)->nullable();
             $table->enum('status', [
                 'Chờ xác nhận',
                 'Chờ lấy hàng',
