3a48caa2f98074d3651f4c0ff2cf17dbe633d459
HoangQuan <104688578+chapilcheapz@users.noreply.github.com>
2025-06-17 15:11:11 +0700

    update: detail order

--- backend/app/Http/Controllers/Api/OrderController.php
+++ backend/app/Http/Controllers/Api/OrderController.php
@@ -63,7 +63,7 @@ class OrderController extends Controller
             $variantAttributes = [];
 
             if ($item->variant) {
-               
+
                 // Lấy thông tin thuộc tính của biến thể
                 $variantAttributes = $item->variant->variantAttributeValues->map(function ($attrValue) {
                     return [
@@ -74,6 +74,7 @@ class OrderController extends Controller
             }
 
             return [
+                'product_id' => $item->product_id,
                 'product_name' => $item->product->name,
                 'product_image' => $item->variant ? $item->variant->image_url : $item->product->image_url,
                 'quantity' => $item->quantity,
