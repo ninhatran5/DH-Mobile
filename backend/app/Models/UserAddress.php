4d0a7adcdeb659583667f944d8a981d46b43b51c
HoangQuan <104688578+chapilcheapz@users.noreply.github.com>
2025-06-18 15:40:31 +0700

    chore: migration user address

--- backend/app/Models/UserAddress.php
+++ backend/app/Models/UserAddress.php
@@ -10,7 +10,7 @@ class UserAddress extends Model
     protected $primaryKey = 'address_id';
 
     protected $fillable = [
-        'user_id', 'recipient_name', 'phone', 'email', 'address', 'ward', 'district', 'city', 'is_default'
+        'user_id', 'recipient_name', 'phone', 'email', 'address', 'is_default'
     ];
 
     public function user()
