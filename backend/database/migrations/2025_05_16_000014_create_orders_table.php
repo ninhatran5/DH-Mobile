f44c4b58e3724ff56ba91c9661246d330f8f8fd1
HoangQuan <104688578+chapilcheapz@users.noreply.github.com>
2025-06-19 14:43:09 +0700

    fix: mail for payment vnpay

--- backend/database/migrations/2025_05_16_000014_create_orders_table.php
+++ backend/database/migrations/2025_05_16_000014_create_orders_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->string('city')->nullable();
             $table->string('phone')->nullable();
             $table->string('email')->nullable();
+            $table->string('customer')->nullable();
 
             $table->enum('status', [
                 'Chờ xác nhận',
