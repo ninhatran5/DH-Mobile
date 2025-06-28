17e02bb2608c568f96e82b6415a71e5f31f79851
LeNguyenXeng <tungnguyenle0909@gmail.com>
2025-06-28 22:46:24 +0700

    update: check role admin

--- frontend/src/components/RequireNotSaleRole.jsx
+++ frontend/src/components/RequireNotSaleRole.jsx
@@ -1,10 +1,12 @@
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
 import ErrorPage from "../pages/Error";
 
 const RequireNotSaleRole = ({ children }) => {
   const { profile } = useSelector((state) => state.profile);
   const role = profile?.user?.role;
+
+  if (!role) return <ErrorPage />;
+
   if (role === "sale") {
     return <ErrorPage />;
   }
