313bda3b3c64869dfa2377ed8875b932274a7389
HoangQuan <104688578+chapilcheapz@users.noreply.github.com>
2025-07-02 14:17:15 +0700

    update: api list loyaltyTier

--- backend/app/Http/Controllers/Api/LoyaltyTierController.php
+++ backend/app/Http/Controllers/Api/LoyaltyTierController.php
@@ -10,9 +10,22 @@ class LoyaltyTierController extends Controller
 {
     public function index()
     {
+        $LoyaltyTier = LoyaltyTier::orderBy('min_points')->get();
+
+        if ($LoyaltyTier->isEmpty()) {
+            return response()->json([
+                'message' => 'Chưa có cấp bậc thành viên nào được tạo.',
+            ]);
+        }
+
+        $LoyaltyTier = $LoyaltyTier->map(function ($tier) {
+            $tier->discount_percent = rtrim(rtrim(number_format($tier->discount_percent, 2, '.', ''), '0'), '.') . '%';
+            return $tier;
+        });
+
         return response()->json([
             'message' => 'Danh sách cấp bậc thành viên',
-            'data' => LoyaltyTier::orderBy('min_points')->get()
+            'data' => $LoyaltyTier
         ]);
     }
 }
