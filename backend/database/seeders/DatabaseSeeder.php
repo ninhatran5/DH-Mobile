890cbdd7276f4e97d69cba75a85613e227cc50db
HoangQuan <104688578+chapilcheapz@users.noreply.github.com>
2025-07-02 14:28:19 +0700

    feat: seeder loyalty tier

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -33,6 +33,7 @@ class DatabaseSeeder extends Seeder
             // VoucherSeeder::class,
             ChatbotSeeder::class,
             PaymentMethodSeeder::class,
+            LoyaltyTierSeeder::class,
            
         ]);
     }
