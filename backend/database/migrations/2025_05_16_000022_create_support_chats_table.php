6e04cc7ce0fb3a8b3c9aa94886e6f224c97ea36e
ninhatran5 <ninhtnph49084@gmail.com>
2025-07-07 12:07:51 +0700

    fix: Update and fix live chat section

--- backend/database/migrations/2025_05_16_000022_create_support_chats_table.php
+++ backend/database/migrations/2025_05_16_000022_create_support_chats_table.php
@@ -1,26 +1,30 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('support_chats', function (Blueprint $table) {
             $table->id('chat_id');
-            $table->unsignedBigInteger('customer_id')->nullable();
-            $table->unsignedBigInteger('staff_id')->nullable();
+            $table->unsignedBigInteger('customer_id')->nullable(); // người nhắn (user role = customer)
+            $table->unsignedBigInteger('staff_id')->nullable();    // người trả lời (role = admin hoặc sale)
+            $table->enum('sender', ['customer', 'admin', 'sale']); // người gửi là ai
             $table->text('message')->nullable();
-            $table->enum('sender', ['customer', 'staff']);
             $table->dateTime('sent_at')->useCurrent();
-            $table->boolean('is_read')->default(0);
+            $table->boolean('is_read')->default(false);
 
-            $table->foreign('customer_id')->references('user_id')->on('users');
-            $table->foreign('staff_id')->references('user_id')->on('users');
+
+            // Foreign keys
+            $table->foreign('customer_id')->references('user_id')->on('users')->onDelete('set null');
+            $table->foreign('staff_id')->references('user_id')->on('users')->onDelete('set null');
         });
     }
+
 
     public function down(): void
     {
