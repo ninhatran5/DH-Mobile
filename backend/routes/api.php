d5a3c00a90ec12d21c08a817fdc5ba2b5a5a5205
ninhatran5 <ninhtnph49084@gmail.com>
2025-07-20 19:53:56 +0700

    feat: add router unread-count id

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -312,6 +312,7 @@ Route::middleware('auth:sanctum')->prefix('support-chats')->group(function () {
     Route::get('/unread-count', [ChatLiveController::class, 'getUnreadCount']);     //  Đếm số tin nhắn chưa đọc
     Route::post('/mark-as-read/{chatId}', [ChatLiveController::class, 'markAsRead']);     //  Đánh dấu một tin nhắn là đã đọc
     Route::get('/chat-user-list', [ChatLiveController::class, 'getCustomersChatList']); // danh sách user nhắn tin cho admin và sale 
+    Route::get('/unread-count/{customerId}', [ChatLiveController::class, 'getUnreadCountByCustomerId']); // lấy danh sách tin nhắn chưa đọc theo id 
 });
 // voucher dành cho user
 Route::middleware('auth:sanctum')->group(function () {
