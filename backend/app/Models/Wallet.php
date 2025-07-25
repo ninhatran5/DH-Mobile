f6aebdfd257428539a37a327aacd5941114fc7bd
HoangQuan <104688578+chapilcheapz@users.noreply.github.com>
2025-07-25 22:38:07 +0700

    feat: wallet for user

---
+++ backend/app/Models/Wallet.php
@@ -0,0 +1,21 @@
+<?php 
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+class Wallet extends Model
+{
+    protected $primaryKey = 'wallet_id';
+    protected $fillable = ['user_id', 'balance'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(WalletTransaction::class, 'wallet_id');
+    }
+}
+
+
---
+++ backend/app/Models/WalletTransaction.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\ReturnRequest;
+use Illuminate\Database\Eloquent\Model;
+
+class WalletTransaction extends Model
+{
+    protected $primaryKey = 'transaction_id';
+    protected $fillable = ['wallet_id', 'type', 'amount', 'note', 'return_id'];
+
+    public function wallet()
+    {
+        return $this->belongsTo(Wallet::class, 'wallet_id');
+    }
+
+    public function returnRequest()
+    {
+        return $this->belongsTo(ReturnRequest::class, 'return_id');
+    }
+}
