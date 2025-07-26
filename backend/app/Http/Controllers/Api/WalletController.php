d1480d4426221f45b23e30777477302c281ae653
HoangQuan <104688578+chapilcheapz@users.noreply.github.com>
2025-07-26 14:01:12 +0700

    feat: api get wallet history by wallet_id

--- backend/app/Http/Controllers/Api/WalletController.php
+++ backend/app/Http/Controllers/Api/WalletController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Wallet;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\WalletTransaction;
 
 class WalletController extends Controller
 {
@@ -13,6 +14,16 @@ class WalletController extends Controller
        $user = $request->user();
        $data = Wallet::where('user_id', $user->user_id)
            ->select('wallet_id', 'balance')
+           ->first();
+       return response()->json([
+           'data' => $data
+       ]);
+   }
+
+   public function getHistoryWallet(Request $request, $id)
+   {
+       $data = WalletTransaction::where('wallet_id', $id)
+           ->select('type', 'amount', 'note', 'created_at')
            ->first();
        return response()->json([
            'data' => $data
