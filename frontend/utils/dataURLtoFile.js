fbff70dbe17c057e8fe99fc96a4cb91e3e2246d7
LeNguyenXeng <tungnguyenle0909@gmail.com>
2025-07-30 22:53:51 +0700

    feat: upload image chat with admin in client

---
+++ frontend/utils/dataURLtoFile.js
@@ -0,0 +1,14 @@
+export default function dataURLtoFile(dataurl, filename) {
+  const arr = dataurl.split(',');
+  const mimeMatch = arr[0].match(/:(.*?);/);
+  const mime = mimeMatch ? mimeMatch[1] : 'image/png';
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+
+  return new File([u8arr], filename, { type: mime });
+}
