8a83132b64f04744b9fa1f3b9c499ef82d17b292
ninhatran5 <ninhtnph49084@gmail.com>
2025-08-22 17:31:36 +0700

    style: format code

--- backend/app/Http/Controllers/Api/OrderController.php
+++ backend/app/Http/Controllers/Api/OrderController.php
@@ -1574,9 +1574,6 @@ class OrderController extends Controller
             // Bắn event
             event(new ReturnNotificationCreated($notification));
 
-
-
-
             DB::commit();
 
             // Broadcast event realtime
