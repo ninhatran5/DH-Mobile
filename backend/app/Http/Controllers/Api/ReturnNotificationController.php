d677773be0a037abf5a08711c36cc246f0b480a9
DuyHungw-svip <hungvdph49075@gmail.com>
2025-08-23 14:18:22 +0700

    fix: ReturnNotificationController

--- backend/app/Http/Controllers/Api/ReturnNotificationController.php
+++ backend/app/Http/Controllers/Api/ReturnNotificationController.php
@@ -11,7 +11,7 @@ class ReturnNotificationController extends Controller
     // Lấy danh sách thông báo (ví dụ cho admin)
     public function index()
     {
-        $notifications = ReturnNotification::with('return_request')->orderBy('created_at', 'desc')->get();
+        $notifications = ReturnNotification::with('returnRequest')->orderBy('created_at', 'desc')->get();
         return response()->json($notifications);
     }
 
